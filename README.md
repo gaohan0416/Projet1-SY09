ff8c047076746f81d57a1d54364c6a828753bf56
Eric <ericlloyd19@gmail.com>
2018-04-24 10:56:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Projet 1 de l'UV SY09 - Analyse de données et apprentissage Automatique
---
+++ cuisine.R
@@ -0,0 +1,4 @@
+#Projet 1
+
+# 1 - Cuisine
+recettes <- read.csv("Projet1/donnees/recettes-pays.data", header=T, row.names=1)
