9c35c96d1572d9fd0fd1d922e485c812d7106008
Eric <ericlloyd19@gmail.com>
2018-04-24 17:43:34 +0200

    cah et k-means

--- cuisine.R
+++ cuisine.R
@@ -23,7 +23,34 @@ U <- eigen$vectors
 #calcul des composantes principales
 C <- X %*% U
 
+#extraction des deux premières composantes
 comp1 <- C[,1]
 comp2 <- C[,2]
 comp3 <- C[,3]
-plot(comp1, comp3)
+
+#plan de représentations 1,2
+plot(comp1, comp2, type ="n")
+text(comp1, comp2,rownames(X))
+abline(h=0);abline(v=0)
+
+
+
+#Classification hiérarchique ascendante
+
+#matrice de distances manhattan
+md.dist <- dist(recettes, method = "manhattan")
+md <- as.matrix(md.dist)
+#dendogramme, methode d'agréfation de Ward
+arbre <- hclust(md.dist, method = "ward.D2")
+plot(arbre)
+
+#Algorithme des K-Means
+#k = 3
+k3.means <- kmeans(recettes, centers = 3)
+plot(comp1, comp2, col=c("red","blue","green")[k3.means$cluster])
+
+#k = 5
+k5.means <- kmeans(recettes, centers = 5)
+plot(comp1, comp2, col=c("red","blue","green", "black", "orange")[k5.means$cluster])
+
+
